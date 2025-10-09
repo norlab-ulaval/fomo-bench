296d47c47fd65dd9a6d728a477abf6e1f6071739
boxanm <matej.boxan@gmail.com>
2025-10-09 17:04:22 -0400

    Align with kabsch algorithm

--- src/entrypoints/entrypoint_evaluate.sh
+++ src/entrypoints/entrypoint_evaluate.sh
@@ -2,7 +2,7 @@
 echo "Starting trajectory analysis..."
 
 # The shell finds 'scripts/trajectory_analysis.py' because we are in '/app'.
-COMMAND="python3 scripts/trajectory_analysis.py"
+COMMAND="python3 scripts/trajectory_analysis.py --zero --alignment kabsch"
 
 if [ "$TEST_MODE" == "1" ]; then
     COMMAND="$COMMAND --test"
