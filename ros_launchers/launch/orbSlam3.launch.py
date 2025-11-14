49c5d4f59cd243c1c562e2abec438db4d978fc07
boxanm <matej.boxan@gmail.com>
2025-11-14 14:49:23 -0500

    Double vslam timeout to give it enough time to save big maps

--- ros_launchers/launch/orbSlam3.launch.py
+++ ros_launchers/launch/orbSlam3.launch.py
@@ -71,8 +71,8 @@ def generate_launch_description():
         executable="stereo-inertial",
         name="orbslam3_stereo",
         output="screen",
-        sigterm_timeout="30",  # Wait 30 seconds before escalating to SIGTERM
-        sigkill_timeout="5",  # Wait 5 more seconds before SIGKILL
+        sigterm_timeout="60",  # Wait 30 seconds before escalating to SIGTERM
+        sigkill_timeout="10",  # Wait 5 more seconds before SIGKILL
         arguments=[
             "--ros-args",
             "--log-level",
